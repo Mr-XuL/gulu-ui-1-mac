63a32bd210cf451d6056b7caa803c392db43d3ac
Mr-XuL <3200323709@qq.com>
2020-12-09 14:19:41 +0800

    TopNav封装完毕

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <iv class="topnav">
-      <div class="logo">logo</div>
-      <ul class="menu">
-              <li>菜单1</li>
-              <li>菜单2</li>
-      </ul>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>造轮子UI</h1>
       <h2>一个自己造的小 UI框架</h2>
@@ -18,21 +12,12 @@
   </div>
 </template>
 
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
+</script>
+
 <style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
 </style>
