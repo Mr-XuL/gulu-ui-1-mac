83470f13baafac3e667dc04ffe82af6d1edc4698
Mr-XuL <3200323709@qq.com>
2020-12-09 17:18:24 +0800

    初始化aside 嵌套路由

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <router-view/>  
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-   
-}
+  name: "App",
+};
 </script>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <Topnav />
     <div class="constent">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/Button">Button组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/Dialog">Dialog组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/Tabs">Tabs组件</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
