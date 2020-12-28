9f0f15fef61336e9c8b0ed2060e68d4d8dd4bc5a
Mr-XuL <3200323709@qq.com>
2020-12-28 15:30:35 +0800

    修改配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,7 @@
       <Topnav />
       <div class="banner">
         <h1>UI框架</h1>
-        <h2>一个非常厉害的ui框架
-
-        </h2>
+        <h2>一个非常厉害的ui框架</h2>
         <p class="actions">
           <a href="https://github.com">GitHub</a>
           <router-link to="/doc">开始</router-link>
@@ -14,29 +12,29 @@
       </div>
     </div>
     <div class="features">
-       <ul>
-      <li>
-        <svg>
-          <use xlink:href="#icon-Vue"></use>
-        </svg>
-        <h3>基于 Vue 3</h3>
-        <p>骄傲地使用了 Vue 3 Composition API</p>
-      </li>
-      <li>
-        <svg>
-          <use xlink:href="#icon-ts"></use>
-        </svg>
-        <h3>基于 TypeScript </h3>
-        <p>源代码采用 TypeScript 书写（非严格检查）</p>
-      </li>
-      <li>
-        <svg>
-          <use xlink:href="#icon-light"></use>
-        </svg>
-        <h3>代码易读</h3>
-        <p>每个组件的源代码都极其简洁</p>
-      </li>
-    </ul>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-Vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript</h3>
+          <p>源代码采用 TypeScript 书写</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -61,22 +59,30 @@ $color: #007974;
   // border-bottom-left-radius: 50% 40px;
   // border-bottom-right-radius: 50% 40px;
   clip-path: ellipse(80% 60% at 50% 40%); // 设置圆弧
-
 }
-.features{
- margin: 64px auto;
-  width: 400px;
+.features {
+  margin: 64px auto;
+  padding: 0 16px;
   @media (min-width: 800px) {
     width: 800px;
+    > ul {
+      > li {
+        width: 50%;
+      }
+    }
   }
   @media (min-width: 1200px) {
     width: 1200px;
+    > ul {
+      > li {
+        width: 33.3333%;
+      }
+    }
   }
-  >ul {
+  > ul {
     display: flex;
     flex-wrap: wrap;
-    >li {
-      width: 400px;
+    > li {
       margin: 16px 0;
       display: grid;
       justify-content: start;
@@ -86,19 +92,20 @@ $color: #007974;
         "icon text";
       grid-template-columns: 80px auto;
       grid-template-rows: 1fr auto;
-      >svg {
+      > svg {
         grid-area: icon;
         width: 64px;
         height: 64px;
       }
-      >h3 {
+      > h3 {
         grid-area: title;
         font-size: 28px;
       }
-      >p {
-        grid-area: text
+      > p {
+        grid-area: text;
       }
-    }}
+    }
+  }
 }
 .banner {
   color: $color;
